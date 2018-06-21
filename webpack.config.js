32702bd519ec7f828abd37588be456d7625f56a7
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-06-21 18:25:56 +0900

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    script: './src/js/index.js',
+    style: './src/css/index.css'
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader'],
+      },
+    ],
+  },
+}
